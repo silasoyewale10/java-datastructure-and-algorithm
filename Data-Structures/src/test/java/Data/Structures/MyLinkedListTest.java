6cce6b0f3dceb712a9ecdbf21f6270c41e110176
Silas Oyewale <silasoyewale10@gmail.com>
2020-01-14 11:38:25 -0800

    code challenege 6 all tests passing

--- Data-Structures/src/test/java/Data/Structures/MyLinkedListTest.java
+++ Data-Structures/src/test/java/Data/Structures/MyLinkedListTest.java
@@ -17,14 +17,14 @@ public class MyLinkedListTest {
         MyLinkedList list1 = new MyLinkedList();
 
         //populate the list
-        list1.populateList("Apple");
-        list1.populateList("Orange");
-        list1.populateList("Mango");
-        list1.populateList("Pawpaw");
-        list1.populateList("Strawberry");
-        list1.populateList("Blueberry");
+        list1.append("Apple");
+        list1.append("Orange");
+        list1.append("Mango");
+        list1.append("Pawpaw");
+        list1.append("Strawberry");
+        list1.append("Blueberry");
 
-        System.out.println( list1.toString());
+//        System.out.println( list1.toString());
         assertEquals("{ Apple } -> { Orange } -> { Mango } -> { Pawpaw } -> { Strawberry } -> { Blueberry } -> NULL",list1.toString());
 
 
@@ -32,12 +32,12 @@ public class MyLinkedListTest {
     @Test public void testIncludesMethod() { // checks if the data is contained in the list
         MyLinkedList list1 = new MyLinkedList();
 
-        list1.populateList("Apple");
-        list1.populateList("Orange");
-        list1.populateList("Mango");
-        list1.populateList("Pawpaw");
-        list1.populateList("Strawberry");
-        list1.populateList("Blueberry");
+        list1.append("Apple");
+        list1.append("Orange");
+        list1.append("Mango");
+        list1.append("Pawpaw");
+        list1.append("Strawberry");
+        list1.append("Blueberry");
 
     //expected false
         assertFalse(list1.includes("Guava"));
@@ -46,13 +46,13 @@ public class MyLinkedListTest {
         assertTrue(list1.includes("Apple"));
 
     }
-    @Test public void testInsertMethod() {   //checks if what is provided is inserted into the head of the linked list
+    @Test public void testInsertMethodAndInsertBeforeFirstNode() {   //checks if what is provided is inserted into the head of the linked list
         MyLinkedList list1 = new MyLinkedList();
 
-        list1.populateList("Apple");
-        list1.populateList("Orange");
-        list1.populateList("Mango");
-        list1.populateList("Pawpaw");
+        list1.append("Apple");
+        list1.append("Orange");
+        list1.append("Mango");
+        list1.append("Pawpaw");
 
 
         //insert Potatoes to the head.
@@ -63,9 +63,39 @@ public class MyLinkedListTest {
     }
     @Test public void testEmptyLinkedList() {   //checks if what is provided is inserted into the head of the linked list
         MyLinkedList list1 = new MyLinkedList();
-        list1.populateList("");
+        list1.append("");
         assertEquals("{  } -> NULL", list1.toString());
 
     }
 
+    @Test public void testAppendAndMultipleNodesToTheEndOfTheList() {   //checks if what is provided is inserted into the head of the linked list
+        MyLinkedList list1 = new MyLinkedList();
+        list1.append("gh");
+        list1.append("vb");
+        list1.append("u");
+        list1.toString();
+        assertEquals("{ gh } -> { vb } -> { u } -> NULL", list1.toString());
+
+
+    }
+    @Test public void testInsertBeforeNodeInTheMiddle() {   //checks if what is provided is inserted into the head of the linked list
+        MyLinkedList list1 = new MyLinkedList();
+        list1.append("Tim");
+        list1.append("Blake");
+        list1.append("Jim ");
+//        list1.insertBefore("vb", "new");
+        list1.insertBefore("Blake", "Chelsea");
+
+        assertEquals("{ Tim } -> { Chelsea } -> { Blake } -> { Jim  } -> NULL", list1.toString());
+    }
+    @Test public void testInsertAfterAndAfterLastNode() {   //checks if what is provided is inserted into the head of the linked list
+        MyLinkedList list1 = new MyLinkedList();
+        list1.append("Brittany");
+        list1.append("Ashley");
+        list1.append("Carmille");
+        list1.insertAfter("Carmille", "Langgy");
+        list1.insertAfter("Ashley", "Jessica");
+
+        assertEquals("{ Brittany } -> { Ashley } -> { Jessica } -> { Carmille } -> { Langgy } -> NULL", list1.toString() );
+    }
 }
