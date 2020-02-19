ef1ebb33be0ec14ab0654c29bab40411044aaf77
Silas Oyewale <silasoyewale10@gmail.com>
2020-02-19 13:45:26 -0800

    done with tests too

--- Data-Structures/src/test/java/datastructures/RepeatedWordTest.java
+++ Data-Structures/src/test/java/datastructures/RepeatedWordTest.java
@@ -19,10 +19,25 @@ public class RepeatedWordTest {
         String expected = "it";
         assertEquals(expected, xx.repeatedWord("It was the best of times, it was the worst of times, it was the age of wisdom, it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, it was the season of Light, it was the season of Darkness, it was the spring of hope, it was the winter of despair, we had everything before us, we had nothing before us, we were all going direct to Heaven, we were all going direct the other way – in short, the period was so far like the present period, that some of its noisiest authorities insisted on its being received, for good or for evil, in the superlative degree of comparison only..."));
     }
+
     @Test
-    public void test3(){
+    public void test3() {
         RepeatedWord xx = new RepeatedWord();
-        String expected = "summer";
-        assertEquals(expected, xx.repeatedWord("It was a queer sultry summer the summer they electrocuted the Rosenbergs and I didnt know what I was doing in New York"));
+        String expected = "no duplicates";
+        assertEquals(expected, xx.repeatedWord("It is me"));
     }
+
+    @Test
+    public void test4Empty() {
+        RepeatedWord xx = new RepeatedWord();
+        String expected = "no duplicates";
+        assertEquals(expected, xx.repeatedWord(""));
+    }
+    @Test
+    public void test5SameWords() {
+        RepeatedWord xx = new RepeatedWord();
+        String expected = "it";
+        assertEquals(expected, xx.repeatedWord("It it it it it it "));
+    }
+
 }
