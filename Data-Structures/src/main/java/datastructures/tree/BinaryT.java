f605febb823c3e378ad47ab10ba3d36c1a13a9d4
Silas Oyewale <silasoyewale10@gmail.com>
2020-02-21 11:34:06 -0800

    done with tests added

--- Data-Structures/src/main/java/datastructures/tree/BinaryT.java
+++ Data-Structures/src/main/java/datastructures/tree/BinaryT.java
@@ -7,51 +7,6 @@ import java.util.Stack;
 public class BinaryT {
     public Node root; //all trees must have this root node.
 
-//    public static void main(String [] args){
-//        BinaryT example = new BinaryT();
-//        BinaryT example2 = new BinaryT();
-//
-//        example.createBinaryTree();
-//        example2.createBinaryTree();
-//
-//        BinaryT x = new BinaryT();
-//
-//        x.intersect(example, example2);
-//        System.out.println(x.intersect(example, example2));
-////    example.printTree(example);
-//    }
-//    public void createBinaryTree(){
-//        Node newNode1 = new Node(5);
-//        Node newNode2 = new Node(15);
-//        Node newNode3 = new Node(52);
-//        Node newNode4 = new Node(25);
-//        Node newNode5 = new Node(85);
-//        Node newNode6 = new Node(33);
-//        Node newNode7 = new Node(12);
-//        Node newNode8 = new Node(7);
-//        Node newNode9 = new Node(5);
-//        Node newNod10 = new Node(5);
-//        Node newNode11 = new Node(5);
-//
-//        root = newNode1;
-//
-//
-//        newNode1.leftOffSpring = newNode2;
-//        newNode1.rightOffSpring = newNode3;
-//
-//        newNode1.leftOffSpring.leftOffSpring = newNode4;
-//        newNode1.leftOffSpring.rightOffSpring = newNode5;
-//
-//        newNode1.rightOffSpring.leftOffSpring = newNode6;
-//        newNode1.rightOffSpring.rightOffSpring = newNode7;
-//
-//        newNode1.rightOffSpring.rightOffSpring.leftOffSpring = newNode8;
-//        newNode1.rightOffSpring.rightOffSpring.rightOffSpring = newNode9;
-//
-//        newNode1.leftOffSpring.rightOffSpring.leftOffSpring = newNod10;
-//        newNode1.leftOffSpring.rightOffSpring.rightOffSpring = newNode11;
-//    }
-
     public void printTree(BinaryT s) {
         while (s.root != null) {
             System.out.println(root.value);
@@ -60,7 +15,8 @@ public class BinaryT {
         }
     }
 
-    public ArrayList<Integer> intersect(BinaryT x, BinaryT y) {  //left , me, right
+    public static ArrayList<Integer> intersect(BinaryT x, BinaryT y) {  //left , me, right
+        System.out.println("here");
         ArrayList<Integer> inOrder = new ArrayList<>();
         HashSet<Integer> ans = new HashSet<>();
         Stack<Node> nodes = new Stack<>();
@@ -83,6 +39,7 @@ public class BinaryT {
             Node temp = nodes.pop();
             if (ans.contains(temp.value)) {
                 inOrder.add(temp.value);
+                ans.remove(temp.value);
             }
             if (temp.leftOffSpring != null) {
                 nodes.add(temp.leftOffSpring);
@@ -92,6 +49,7 @@ public class BinaryT {
             }
 
         }
+        System.out.println("inOrder = " + inOrder);
         return inOrder;
     }
 }
